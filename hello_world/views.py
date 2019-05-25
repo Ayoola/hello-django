282d7bfc7969d32bc076a09063bc64d152a06d3c
Ayoola John <ayo.johnsu@gmail.com>
2019-05-25 16:35:39 -0500

    added static and templates

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    dict={
+    'insert-me':'I have been inserted dynamically to the homepage',
+    }
+    return render(request, 'index.html', context=dict)
+
+def hello_world(request):
+    dict={
+    'insert-me':'I have been inserted dynamically to the Hello World Page'
+    }
+    return render(request, 'hello_world/hello.html', context=dict)
