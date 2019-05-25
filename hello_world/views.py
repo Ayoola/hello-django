6e1ecc033b6e395a2ef2ff7f948dff2485390133
Ayoola John <ayo.johnsu@gmail.com>
2019-05-25 16:57:09 -0500

    basic structure setup

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     dict={
-    'insert-me':'I have been inserted dynamically to the homepage',
+    'insertme':'I have been inserted dynamically to the homepage',
     }
     return render(request, 'index.html', context=dict)
 
 def hello_world(request):
     dict={
-    'insert-me':'I have been inserted dynamically to the Hello World Page'
+    'insertme':'I have been inserted dynamically to the Hello World Page'
     }
     return render(request, 'hello_world/hello.html', context=dict)
